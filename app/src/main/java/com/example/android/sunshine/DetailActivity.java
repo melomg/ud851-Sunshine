ee733f0f34af92850b33f77b7707e2f4f5359aff
Melih Gültekin <melih.gultekin@d-teknoloji.com.tr>
2017-11-18 22:21:47 +0300

    TODO done

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,6 +14,12 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        TextView textView = (TextView) findViewById(R.id.tv_weather_data);
+
+        // COMPLETED (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            textView.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+        }
     }
 }
