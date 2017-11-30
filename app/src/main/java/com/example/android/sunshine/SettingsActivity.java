dd876efe5d1f52d73dcd013e9e5e6c848307bf3f
Melih Gültekin <melih.gultekin@d-teknoloji.com.tr>
2017-11-30 21:06:42 +0300

    TODO done

---
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.sunshine;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_settings);
+
+        final ActionBar supportActionBar = this.getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_settings) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
