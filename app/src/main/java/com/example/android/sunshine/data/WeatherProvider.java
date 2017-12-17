2292aff462e2d56eab338389145b785c84b64430
Melih Gültekin <mmelihgultekin@gmail.com>
2017-12-17 13:09:15 +0300

    TODO done

--- app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -293,7 +293,7 @@ public class WeatherProvider extends ContentProvider {
         return cursor;
     }
 
-//  TODO (1) Implement the delete method of the ContentProvider
+//  COMPLETED (1) Implement the delete method of the ContentProvider
     /**
      * Deletes data at a given URI with optional arguments for more fine tuned deletions.
      *
@@ -304,11 +304,23 @@ public class WeatherProvider extends ContentProvider {
      */
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-        throw new RuntimeException("Student, you need to implement the delete method!");
-
-//          TODO (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
-
-//      TODO (3) Return the number of rows deleted
+        int noDeletedRows;
+        // COMPLETED (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
+        switch (sUriMatcher.match(uri)) {
+            case CODE_WEATHER: {
+                SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+                noDeletedRows = db.delete(WeatherContract.WeatherEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+            }
+        }
+        // COMPLETED (3) Return the number of rows deleted
+        if (noDeletedRows > 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return noDeletedRows;
     }
 
     /**
